6e926f9bf2966337fadabd35a9a9b9dc51a6204e
Sleepy-kookoo <americaxalvarez@gmail.com>
2024-07-18 12:09:54 -0400

    Answered Section 3, working on Section 2

--- src/1-inheritance-polymorphism.js
+++ src/1-inheritance-polymorphism.js
@@ -1,5 +1,19 @@
 class Phone {
+  constructor(phoneNumber) {
+    this.phoneNumber = phoneNumber;
+    this.contacts = [];
+  }
 
+  addContact(newContact) {
+    if (!newContact.name || !newContact.phoneNumber) return `Invalid`
+    if (newContact.phoneNumber) return `Invalid`
+    this.contacts.push(newContact)
+    return `${newContact.name} added.`
+  }
+
+  removeContact(name) {
+    this.contacts.splice(this.contacts.findIndex((contact) => contact.name === name), 1)
+  }
 }
 
 class AppleIPhone {
--- src/2-factory-function.js
+++ src/2-factory-function.js
@@ -1,6 +1,19 @@
-const bankAccountFactory = () => {
-
-};
+const bankAccountFactory = (balance) => ({
+  checkBalance: function () {
+    if (!balance) balance = 0
+    return balance
+  },
+  add: function (amount) {
+    const startingBalance = balance;
+    balance = (startingBalance + amount)
+    console.log(`$${amount} added.`)
+  },
+  subtract: function (amount) {
+    const startingBalance = balance;
+    balance = (startingBalance - amount)
+    console.log(`$${amount} subtracted.`)
+  }
+});
 
 module.exports = {
   bankAccountFactory,
