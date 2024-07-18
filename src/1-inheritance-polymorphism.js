618cc78434f383185a1270b8183fe22b91c60065
Sleepy-kookoo <americaxalvarez@gmail.com>
2024-07-18 13:26:20 -0400

    Missing 3 test

--- src/1-inheritance-polymorphism.js
+++ src/1-inheritance-polymorphism.js
@@ -6,7 +6,7 @@ class Phone {
 
   addContact(newContact) {
     if (!newContact.name || !newContact.phoneNumber) return `Invalid`
-
+    if (newContact.phoneNumber.length !== 10) return `Invalid`
     // Need to work on this!
     this.contacts.push(newContact)
     return `${newContact.name} added.`
@@ -25,6 +25,8 @@ class Phone {
   makeCall(name) {
     if ((this.contacts.find((contact) => contact.name === name))) {
       return `Calling ${name}...`
+    } else if (name.length !== 10) {
+      return `Invalid`
     } else {
       return `Calling ${name}...`
     }
@@ -37,6 +39,7 @@ class AppleIPhone extends Phone {
     this.model = model
   }
   sendIMessage(phone, message) {
+    if (phone) return
     return `Message: ${message} - sent from ${this.model}`
   }
 }
